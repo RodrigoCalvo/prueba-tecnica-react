6b981d3a7f027289906e5dafd461417a1ed3f632
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2024-11-21 18:15:52 +0100

    feat: add basic ensambled structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Login from './modules/Login';
-import { getCharactersList } from './api/characters';
+import { useEffect } from "react";
+import "./App.css";
+import LoginScreen from "./modules/login-screen";
+import { useController } from "./controller/useController";
+import { Header } from "./modules/header";
+import { CharactersList } from "./modules/characters-list";
+import { ListNavigation } from "./modules/list-navigation";
+import { Footer } from "./modules/footer";
 
 function App() {
-    useEffect(() => {
-        getCharactersList(0).then((data) => console.log(data));
-    });
-    const [logged, setLogged] = useState(false);
-    return <div className="App">{!logged && <Login setter={setLogged} />}</div>;
+  const { loginState, loadCharactersList } = useController();
+  useEffect(() => {
+    loadCharactersList();
+  }, [loadCharactersList]);
+
+  return (
+    <div className="App">
+      {!loginState.logged ? (
+        <LoginScreen />
+      ) : (
+        <>
+          <Header />
+          <CharactersList />
+          <ListNavigation />
+          <Footer />
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
