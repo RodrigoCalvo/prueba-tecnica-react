ba2857a4b1dd302a1592e7428c7ce325d8913af8
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2024-11-22 00:15:18 +0100

    feat: add detail module

--- frontend/src/modules/character-detail/character-detail.tsx
+++ frontend/src/modules/character-detail/character-detail.tsx
@@ -1,3 +1,48 @@
+import { useController } from "../../controller/useController";
+import "./character-detail.css";
+
 export const CharacterDetail = () => {
-  return <></>;
+  const { selectedCharacter } = useController();
+
+  return (
+    <>
+      {selectedCharacter ? (
+        <div className="character-detail">
+          <header className="character-detail__header">
+            <picture className="character-detail__image-container">
+              <img
+                className="character-detail__image"
+                src={selectedCharacter.image}
+                alt={selectedCharacter.name}
+              />
+            </picture>
+            <h2 className="character-detail__name">{selectedCharacter.name}</h2>
+          </header>
+          <section className="character-detail__info">
+            <h3 className="character-detail__subtitle">Descripción:</h3>
+            <p className="character-detail__description">
+              {selectedCharacter.description ??
+                "No existe descripción de este personaje"}
+            </p>
+            <h3 className="character-detail__list-title">Listado de cómics:</h3>
+            <ul className="character-detail__comics-list">
+              {selectedCharacter.comics.count &&
+                selectedCharacter.comics.titles.map((comicTitle, index) => (
+                  <li
+                    key={`${comicTitle}-${index}`}
+                    className="character-detail__comic-item"
+                  >
+                    {comicTitle}
+                  </li>
+                ))}
+            </ul>
+          </section>
+        </div>
+      ) : (
+        <p className="character-detail__error-message">
+          Por favor accede a la ficha a través del listado
+        </p>
+      )}
+    </>
+  );
 };
