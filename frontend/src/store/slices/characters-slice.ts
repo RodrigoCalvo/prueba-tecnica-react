2ab639a23aa9e2ae7770dfc1299b50899fe4217b
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2024-11-21 16:18:12 +0100

    feat: add characters redux actions

--- frontend/src/store/slices/characters-slice.ts
+++ frontend/src/store/slices/characters-slice.ts
@@ -1,16 +1,50 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { CharactersInitalStateVM } from '../../models/initial-state';
 
-const initialState = {};
+const initialState: CharactersInitalStateVM = {
+  charactersList: {
+    attributionHTML: '',
+    data: [],
+    pagination: {
+      elements: 0,
+      page: 0,
+    },
+  },
+  selectedCharacter: {
+    id: 0,
+    name: '',
+    description: '',
+    image: '',
+    comics: {
+      count: {
+        total: 0,
+        displayed: 0,
+      },
+      titles: [],
+    },
+  },
+};
 
 export const charactersSlice = createSlice({
   name: 'characters',
   initialState,
 
   reducers: {
-    myFunction: () => {},
+    setCharactersList: (
+      state,
+      { payload }: PayloadAction<CharactersInitalStateVM['charactersList']>
+    ) => {
+      state.charactersList = payload;
+    },
+    setSelectedCharacter: (
+      state,
+      { payload }: PayloadAction<CharactersInitalStateVM['selectedCharacter']>
+    ) => {
+      state.selectedCharacter = payload;
+    },
   },
 });
 
-export const { myFunction: nada } = charactersSlice.actions;
+export const { setCharactersList } = charactersSlice.actions;
 
 export default charactersSlice.reducer;
