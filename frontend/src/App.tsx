d6bb795f76588912680ca2716f62b29c25ba27ca
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2024-11-25 12:14:38 +0100

    feat: move data fetch to home subpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,10 @@
-import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { useController } from "./controller/useController";
 import { Home } from "./pages/home";
 import { Login } from "./pages/login";
 import { Character } from "./pages/character";
 
 function App() {
-  const { loadCharactersList, selectedPage } = useController();
-
-  useEffect(() => {
-    loadCharactersList(selectedPage);
-  }, [loadCharactersList, selectedPage]);
-
   const routerOptions: Array<{ path: string; label: string; page: any }> = [
     { path: "/", label: "Login", page: <Login></Login> },
     { path: "/home", label: "Inicio", page: <Home></Home> },
