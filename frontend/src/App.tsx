9bb7c8d72095a60c8fe8cba8980ee9df455bd6d7
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2024-11-25 14:47:22 +0100

    feat: add logged route protection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,28 @@ import "./App.css";
 import { Home } from "./pages/home";
 import { Login } from "./pages/login";
 import { Character } from "./pages/character";
+import { LoginValidate } from "./modules/login-validate/login-validate";
 
 function App() {
   const routerOptions: Array<{ path: string; label: string; page: any }> = [
     { path: "/", label: "Login", page: <Login></Login> },
-    { path: "/home", label: "Inicio", page: <Home></Home> },
+    {
+      path: "/home",
+      label: "Inicio",
+      page: (
+        <LoginValidate>
+          <Home></Home>
+        </LoginValidate>
+      ),
+    },
     {
       path: "/character/:id",
       label: "Detalles",
-      page: <Character></Character>,
+      page: (
+        <LoginValidate>
+          <Character></Character>
+        </LoginValidate>
+      ),
     },
   ];
   return (
