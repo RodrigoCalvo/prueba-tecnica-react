70f9d76f1a213fd20a3595451405dfcec4e7280c
RodrigoCalvo <99681649+RodrigoCalvo@users.noreply.github.com>
2024-11-25 12:15:55 +0100

    feat: add character fetch to character subpage

--- frontend/src/pages/character.tsx
+++ frontend/src/pages/character.tsx
@@ -1,8 +1,30 @@
+import { useEffect } from "react";
 import { CharacterDetail } from "../modules/character-detail/character-detail";
 import { Footer } from "../modules/footer/footer";
 import { Header } from "../modules/header/header";
+import { useController } from "../controller/useController";
+import { MARVEL_CHAR_ID_LENGHT } from "../config/apiConfig";
+import { getCharacter } from "../api/characters";
 
 export const Character = () => {
+  const { selectedCharacter, setSelectedCharacter } = useController();
+  useEffect(() => {
+    const currentUrl = window.location.href.split("?")[0];
+    const characterId = currentUrl.split("/").at(-1);
+    if (
+      !characterId ||
+      characterId.length !== MARVEL_CHAR_ID_LENGHT ||
+      isNaN(Number(characterId)) ||
+      Number(characterId) <= 0 ||
+      !Number.isInteger(Number(characterId))
+    )
+      return;
+    if (!selectedCharacter.id || Number(characterId) !== selectedCharacter.id) {
+      getCharacter(Number(characterId)).then((data) =>
+        setSelectedCharacter(data?.character)
+      );
+    }
+  }, [selectedCharacter.id, setSelectedCharacter]);
   return (
     <>
       <Header></Header>
